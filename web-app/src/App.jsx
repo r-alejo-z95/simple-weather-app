2535b0fc7704694dc0836c77ad7a71a8e94b73ef
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-03-08 00:28:24 -0500

    Add Card component

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="font-bold text-5xl">Hello World</h1>
+      <div className="flex flex-col m-8 justify-center content-center gap-6">
+        <h1 className="font-bold text-4xl just text-center">Simple Weather</h1>
+
+        <div className="flex flex-row justify-evenly">
+          <Card className="flex flex-col ">
+            <CardHeader>
+              <CardTitle>City</CardTitle>
+            </CardHeader>
+            <CardContent>Weather animation</CardContent>
+            <CardContent>Temperature</CardContent>
+            <Button>C°</Button>
+          </Card>
+        </div>
       </div>
     </>
   );
