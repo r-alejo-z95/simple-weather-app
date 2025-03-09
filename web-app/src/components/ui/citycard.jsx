123fa1645eb25f0f2ddeadbb21049bf3fdb719d4
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-03-09 00:46:39 -0500

    Update CityCard component to accept location prop and adjust layout in App

--- web-app/src/components/ui/citycard.jsx
+++ web-app/src/components/ui/citycard.jsx
@@ -2,19 +2,17 @@ import * as React from "react";
 import { Card, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
-function CityCard() {
+function CityCard({ location }) {
   return (
     <>
-      <div id="card" className="flex flex-row justify-evenly gap-1">
-        <Card>
-          <CardTitle>City</CardTitle>
-          <CardContent>Weather animation</CardContent>
-          <CardContent>Temperature</CardContent>
-          <CardContent>
-            <Button>C°</Button>
-          </CardContent>
-        </Card>
-      </div>
+      <Card>
+        <CardTitle>{location}</CardTitle>
+        <CardContent>Weather animation</CardContent>
+        <CardContent>Temperature</CardContent>
+        <CardContent>
+          <Button>C°</Button>
+        </CardContent>
+      </Card>
     </>
   );
 }
