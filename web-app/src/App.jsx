ddaf2359d8e2076fe4907353b13cc91c051d52b5
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-03-08 22:39:42 -0500

    Add AddCityButton component and update App layout and styling

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,22 +1,30 @@
 import "./App.css";
 import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
 import { Button } from "./components/ui/button";
+import { AddCityButton } from "./components/ui/addcitybutton";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col m-8 justify-center content-center gap-6">
-        <h1 className="font-bold text-4xl just text-center">Simple Weather</h1>
+      <div className="flex flex-col m-10 gap-5">
+        <div className="flex flex-col place-items-center gap-8">
+          <h1 className="font-bold text-4xl ">Simple Weather</h1>
+          <p>Add up to 3 cities</p>
+        </div>
 
-        <div className="flex flex-row justify-evenly">
-          <Card className="flex flex-col ">
-            <CardHeader>
-              <CardTitle>City</CardTitle>
-            </CardHeader>
+        <div className="flex flex-row justify-evenly gap-1">
+          <Card>
+            <CardTitle>City</CardTitle>
             <CardContent>Weather animation</CardContent>
             <CardContent>Temperature</CardContent>
-            <Button>C°</Button>
+            <CardContent>
+              <Button>C°</Button>
+            </CardContent>
           </Card>
+        </div>
+
+        <div className="flex justify-center">
+          <AddCityButton>Add city</AddCityButton>
         </div>
       </div>
     </>
